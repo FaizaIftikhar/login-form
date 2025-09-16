ecef58d5d53d5dee39ef90c17b209f2f630bc86d
FaizaIftikhar <iamfaiza8o9@gmail.com>
2025-09-16 15:46:31 +0100

    Fetching data using useEffect and added Loaded state and Error State

--- react-form/src/UsersList.jsx
+++ react-form/src/UsersList.jsx
@@ -1,6 +1,23 @@
 import {useEffect,useState} from 'react'
 import axios from 'axios';
 function UserList(){
+    const[users,setUsers]=useState([])
+    const[loading,setLoading]=useState(true)
+    const[error,setError]=useState(null)
+
+    useEffect(()=>{
+        axios.get('https://jsonplaceholder.typicode.com/users')
+        .then(res=>{
+            setUsers(res.data)
+            setLoading(false)
+        })
+        .catch(()=>{
+            setError("Something went wrong")
+            setLoading(false)
+        }
+
+        )
+    })
     return(
         <>
         </>
