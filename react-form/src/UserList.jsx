21d776404e1f11c55af58e8ed134590741cf859e
FaizaIftikhar <iamfaiza8o9@gmail.com>
2025-09-19 13:10:18 +0100

    Display welcome message in UserList using UserContext

--- react-form/src/UserList.jsx
+++ react-form/src/UserList.jsx
@@ -1,14 +1,18 @@
+// src/UsersList.js
 import React from "react";
 import { useUser } from "./UserContext";
+import { useTheme } from "./ThemeContext";
+import "./UserList.css";
 
 export default function UsersList() {
   const { user, logout } = useUser();
+  const { theme } = useTheme();
 
   return (
-    <div>
+    <div className={`users-list ${theme}`}>
       {user ? (
         <>
-          <p>Welcome back, {user.username}!</p>
+          <p>👉 Welcome back, {user.username}!</p>
           <button onClick={logout}>Logout</button>
         </>
       ) : (
