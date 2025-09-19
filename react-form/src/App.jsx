9faa0e800616fce58953de9d84588ff6728243b2
FaizaIftikhar <iamfaiza8o9@gmail.com>
2025-09-19 13:07:36 +0100

    Add ThemeContext with light/dark mode toggle and wrap app with UserProvider and ThemeProvider

--- react-form/src/App.jsx
+++ react-form/src/App.jsx
@@ -1,14 +1,36 @@
-//import LoginForm from "./LoginForm";
-import React from "react"; 
-//import UpgradeLoginForm from "./UpgradeLoginForm";
-import Objects from "./Objects"
+// src/App.js
+import React, { useEffect } from "react";
+import { UserProvider } from "./UserContext";
+import { ThemeProvider, useTheme } from "./ThemeContext";
+import LoginForm from "./LoginForm";
+import UsersList from "./UserList";
+import "./App.css";
 
-function App() {
+function AppContent() {
+  const { theme, toggleTheme } = useTheme();
+
+  // Apply theme class to body
+  useEffect(() => {
+    document.body.className = theme;
+  }, [theme]);
+
   return (
-    <>
-      <Objects/>
-    </>
+    <div className="App">
+      <button className="theme-toggle" onClick={toggleTheme}>
+        Switch to {theme === "light" ? "Dark" : "Light"} Mode
+      </button>
+      <LoginForm />
+      <UsersList />
+    </div>
   );
 }
 
-export default App;
+export default function App() {
+  return (
+    <UserProvider>
+      <ThemeProvider>
+        <AppContent />
+      </ThemeProvider>
+    </UserProvider>
+  );
+}
