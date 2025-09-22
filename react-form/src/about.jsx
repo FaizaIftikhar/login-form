9d083f2899b9952668a6034ff77be0130742161d
FaizaIftikhar <iamfaiza8o9@gmail.com>
2025-09-22 20:26:15 +0100

    Creating About page and adding small description in it

---
+++ react-form/src/about.jsx
@@ -0,0 +1,13 @@
+// src/About.js
+import React from "react";
+
+export default function About() {
+  return (
+    <div style={{ padding: "2rem", textAlign: "center" }}>
+      <h2>About This App</h2>
+      <p>
+        This is the about page
+      </p>
+    </div>
+  );
+}
